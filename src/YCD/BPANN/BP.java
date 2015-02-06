bb2de8966289ee738d4fc9ab6800ff917ec4a924
yinchuandong <yincd520@sina.com>
2015-02-06 12:09:13 +0800

    update in and out matrix

--- src/YCD/BPANN/BP.java
+++ src/YCD/BPANN/BP.java
@@ -1,5 +1,6 @@
 package YCD.BPANN;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class BP {
@@ -30,40 +31,75 @@ public class BP {
 	
 	private Random random;
 	
-	public BP(int inSize, int hideSize, int outSize, double eta, double momentum, double b){
-		this.in = new double[inSize];
-		this.hide = new double[hideSize];
-		this.out = new double[outSize];
-		this.target = new double[outSize];
+	public BP(int inSize, int hideSize, int outSize, double eta, double momentum){
+		this.in = new double[inSize + 1];
+		this.hide = new double[hideSize + 1];
+		this.out = new double[outSize + 1];
+		this.target = new double[outSize + 1];
 		
-		this.hideErr = new double[hideSize];
-		this.outErr = new double[outSize];
+		this.hideErr = new double[hideSize + 1];
+		this.outErr = new double[outSize + 1];
 		
 		//increasing row size is to add W0*X0 as threshold
-		this.inHideW = new double[hideSize][inSize + 1];
-		this.hideOutW = new double[outSize][hideSize + 1];
+		this.inHideW = new double[hideSize + 1][inSize + 1];
+		this.hideOutW = new double[outSize + 1][hideSize + 1];
 		
 		this.eta = eta;
 		this.momentum = momentum;
-		this.b = b;
 		
 		randomInit(inHideW);
-		randomInit(hideOutW);;
+		randomInit(hideOutW);
 	}
 	
 	private void randomInit(double[][] weight){
-		for (int i = 0; i < weight.length; i++) {
-			for (int j = 0; j < weight[i].length; j++) {
+		for (int j = 0; j < weight.length; j++) {
+			for (int i = 1; i < weight[j].length; i++) {
 				double real = random.nextDouble();
-				weight[i][j] = real > 0.5 ? real : - real;
+				weight[j][i] = real > 0.5 ? real : - real;
 			}
 		}
 	}
 	
-	public void train(double[] in, double[] target){
-		this.in = in;
-		this.target = target;
-		
+	public void train(double[] trainData, double[] target){
+		loadTrain(trainData);
+		loadTarget(target);
+		forward();
+		backProprogate();
+		adjustWeight();
+	}
+	
+	public double[] predict(double[] inData){
+		if(inData.length != in.length - 1){
+			throw new IllegalArgumentException("In Size Do Not Match.");
+		}
+		System.arraycopy(inData, 0, in, 1, inData.length);
+		forward();
+		return getNetworkOutput();
+	}
+	
+	/**
+	 * return the value of output layer
+	 */
+	private double[] getNetworkOutput() {
+		int len = out.length;
+		double[] temp = new double[len - 1];
+		for (int i = 1; i != len; i++)
+			temp[i - 1] = out[i];
+		return temp;
+	}
+	
+	private void loadTrain(double[] trainData){
+		if(trainData.length != in.length - 1){
+			throw new IllegalArgumentException("Train Size Do Not Match.");
+		}
+		System.arraycopy(trainData, 0, in, 1, trainData.length);
+	}
+	
+	private void loadTarget(double[] t){
+		if(t.length != target.length - 1){
+			throw new IllegalArgumentException("Train Size Do Not Match.");
+		}
+		System.arraycopy(t, 0, target, 1, t.length);
 	}
 	
 	private void forward(){
@@ -82,9 +118,10 @@ public class BP {
 	}
 	
 	private void forward(double[] in, double[] out, double[][] w){
-		for (int j = 0; j < w.length; j++) {
-			double sum = b * w[j][0];
-			for (int i = 1; i < w[j].length; i++) {
+		in[0] = 1.0;
+		for (int j = 1; j < out.length; j++) {
+			double sum = 0.0;
+			for (int i = 0; i < in.length; i++) {
 				sum += w[j][i] * in[i];
 			}
 			out[j] = sigmoid(sum);
@@ -93,7 +130,7 @@ public class BP {
 	
 	private void calcOutErr(){
 		double sum = 0.0;
-		for (int i = 0; i < outErr.length; i++) {
+		for (int i = 1; i < outErr.length; i++) {
 			double t = target[i];
 			double o = out[i];
 			double e = o * (1 - o) * (t - o);
@@ -105,7 +142,7 @@ public class BP {
 	
 	private void calcHideError(){
 		double sum = 0.0;
-		for (int i = 0; i < hideErr.length; i++) {
+		for (int i = 1; i < hideErr.length; i++) {
 			double o = hide[i];
 			double tmpS = 0.0;
 			for (int j = 0; j < outErr.length; j++) {
@@ -119,10 +156,9 @@ public class BP {
 	}
 	
 	private void adjustWeight(double[] in, double[] delta, double[][] w){
-		for (int j = 0; j < delta.length; j++) {
+		for (int j = 1; j < delta.length; j++) {
 			for (int i = 0; i < in.length; i++) {
-				double xji = (i == 0 ? b : in[i]);
-				double deltaW = eta * delta[j] * xji + momentum * w[j][i];
+				double deltaW = eta * delta[j] * in[i] + momentum * w[j][i];
 				w[j][i] += deltaW;
 			}
 		}
